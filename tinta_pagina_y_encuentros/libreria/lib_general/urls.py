181e2dfc797a636e1bdb9206a6a9988bc9ab268f
Diana Patiño <e.narvaez.paz@gmail.com>
2025-04-28 01:14:16 -0500

    index base

--- tinta_pagina_y_encuentros/libreria/lib_general/urls.py
+++ tinta_pagina_y_encuentros/libreria/lib_general/urls.py
@@ -3,4 +3,9 @@ from django.urls import path, include
 from . import views
 from django.contrib.auth import views as auth_views
  
-urlpatterns = []
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('registro/', views.registro, name='registro'),
+    path('libros/', views.libros, name='libros'),
+    ]
